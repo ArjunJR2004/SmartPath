545c9f5945cbedfaf4fd7cdc3062c2b9b1d4e279
ArjunJR2004 <arjunjr.dev@gmail.com>
2026-07-31 19:46:23 +0530

    Implement reusable layout with footer and nested routing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,29 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 
+import Layout from "../components/layout/Layout";
 import Home from "../pages/Home/Home";
+import Courses from "../pages/Courses/Courses";
+import WishList from "../pages/WishList/WishList";
+import Dashboard from "../pages/Dashboard/Dashboard";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import Courses from "../pages/Courses/Courses";
-import CourseDetails from "../pages/CourseDetails/CourseDetails";
-import Wishlist from "../pages/WishList/WishList";
-import Profile from "../pages/Profile/Profile";
-import Settings from "../pages/Settings/Settings";
-import NotFound from "../pages/NotFound/NotFound";
 
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/course/:id" element={<CourseDetails />} />
-            <Route path="/wishlist" element={<Wishlist />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="*" element={<NotFound />} />
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/wishlist" element={<WishList />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Route>
         </Routes>
     );
 }

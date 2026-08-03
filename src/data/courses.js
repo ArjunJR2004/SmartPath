a0cbed0f7e5f3b83d92c3c7cc903a4b54e2d32e8
ArjunJR2004 <arjunjr.dev@gmail.com>
2026-08-03 21:58:52 +0530

    Added python programming course

--- src/data/courses.js
+++ src/data/courses.js
@@ -2,6 +2,7 @@ import reactCourse from "../assets/images/courses/reactCourse.jpg";
 import javaCourse from "../assets/images/courses/javaCourse.jpg";
 import springBoot from "../assets/images/courses/springBoot.jpg";
 import javaScript from "../assets/images/courses/javaScript.jpg";
+import python from "../assets/images/courses/python.jpg";
 
 const courses = [
     {
@@ -31,6 +32,13 @@ const courses = [
         instructor: "Robert Brown",
         price: "₹699",
         image: javaScript
+    },
+    {
+        id: 5,
+        title: "Python Programming",
+        instructor: "Kiran Kumar",
+        price: "₹699",
+        image: python
     }
 ];
 
